e0bc69aae664ec17829d74fc6a95f85bdde11811
kevin <kevin.vandenbreemen@gmail.com>
2023-11-07 18:50:21 -0800

    Pull in the latest version of java parser

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 
-    val javaParserVersion = "3.22.1"
+    val javaParserVersion = "3.25.6"
     implementation("com.github.javaparser:javaparser-symbol-solver-core:$javaParserVersion")
 
     val kluentVersion = "1.68"
